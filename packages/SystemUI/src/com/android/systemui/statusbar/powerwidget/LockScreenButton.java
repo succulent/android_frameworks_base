e63c7a71988f8167a5457971f004a42826baf7c5
Scott Brady <sbradymobile@gmail.com>
2012-04-29 11:50:53 -0400

    Lockscreen button update
    Change-Id: Ic2867e7ccffb4129fedd457bc4e54ca9518b25ff

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/LockScreenButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/LockScreenButton.java
@@ -2,30 +2,29 @@ package com.android.systemui.statusbar.powerwidget;
 
 import com.android.systemui.R;
 
-import android.app.Activity;
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.view.Gravity;
-import android.widget.Toast;
+
+import com.android.internal.widget.LockPatternUtils;
 
 public class LockScreenButton extends PowerButton {
 
-    private static Boolean LOCK_SCREEN_STATE = null;
+    private LockPatternUtils mLockPatternUtils;
+    private boolean mLockScreenState;
 
-    private KeyguardLock mLock = null;
-
-    public LockScreenButton() { mType = BUTTON_LOCKSCREEN; }
+    public LockScreenButton() {
+        mType = BUTTON_LOCKSCREEN;
+    }
 
     @Override
     protected void updateState() {
-        getState();
-        if (LOCK_SCREEN_STATE == null) {
+        if (mLockPatternUtils == null) {
+            mLockPatternUtils = new LockPatternUtils(mView.getContext());
+        }
+        mLockScreenState = !mLockPatternUtils.isLockScreenDisabled();
+        if (mLockPatternUtils.isSecure()) {
             mIcon = R.drawable.stat_lock_screen_off;
             mState = STATE_INTERMEDIATE;
-        } else if (LOCK_SCREEN_STATE) {
+        } else if (mLockScreenState) {
             mIcon = R.drawable.stat_lock_screen_on;
             mState = STATE_ENABLED;
         } else {
@@ -36,23 +35,18 @@ public class LockScreenButton extends PowerButton {
 
     @Override
     protected void toggleState() {
-        Context context = mView.getContext();
-        getState();
-        if(LOCK_SCREEN_STATE == null) {
-            Toast msg = Toast.makeText(context, "Not yet initialized", Toast.LENGTH_LONG);
-            msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
-            msg.show();
-        } else {
-            getLock(context);
-            if (mLock != null) {
-                if (LOCK_SCREEN_STATE) {
-                  mLock.disableKeyguard();
-                  LOCK_SCREEN_STATE = false;
-                } else {
-                  mLock.reenableKeyguard();
-                  LOCK_SCREEN_STATE = true;
-                }
+        if (mLockPatternUtils == null) {
+            mLockPatternUtils = new LockPatternUtils(mView.getContext());
+        }
+        mLockScreenState = !mLockPatternUtils.isLockScreenDisabled();
+        if (!mLockPatternUtils.isSecure()) {
+            if (mLockScreenState) {
+                mLockPatternUtils.setLockScreenDisabled(true);
+            } else {
+                mLockPatternUtils.setLockScreenDisabled(false);
             }
+        } else {
+            launchSecuritySettings();
         }
 
         // we're handling this, so just update our buttons now
@@ -62,27 +56,14 @@ public class LockScreenButton extends PowerButton {
 
     @Override
     protected boolean handleLongClick() {
+        launchSecuritySettings();
+        return true;
+    }
+
+    private void launchSecuritySettings() {
         Intent intent = new Intent("android.settings.SECURITY_SETTINGS");
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mView.getContext().startActivity(intent);
-        return true;
-    }
-
-    private KeyguardLock getLock(Context context) {
-        if (mLock == null) {
-            KeyguardManager keyguardManager = (KeyguardManager)context.
-                    getSystemService(Activity.KEYGUARD_SERVICE);
-            mLock = keyguardManager.newKeyguardLock(Context.KEYGUARD_SERVICE);
-        }
-        return mLock;
-    }
-
-    private static boolean getState() {
-        if (LOCK_SCREEN_STATE == null) {
-            LOCK_SCREEN_STATE = true;
-        }
-        return LOCK_SCREEN_STATE;
     }
 }
-
