03188026caab8acd0b65051e5bb735cb5273b0cf
Scott <sbradymobile@gmail.com>
2012-09-30 14:01:40 -0400

    Add hasNavBar check to settings observer
    Change-Id: I5d71480fb97968fad2dc5f8b7db235e60a101894

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -193,9 +193,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     private boolean mShowNotificationCounts;
 
     public void start() {
-        SettingsObserver observer = new SettingsObserver(mHandler);
-        observer.observe(mContext);
-
         mDisplay = ((WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE))
                 .getDefaultDisplay();
 
@@ -206,6 +203,9 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         mWindowManager = IWindowManager.Stub.asInterface(
                 ServiceManager.getService(Context.WINDOW_SERVICE));
+
+        SettingsObserver observer = new SettingsObserver(mHandler, mWindowManager);
+        observer.observe(mContext);
 
         mBarService = IStatusBarService.Stub.asInterface(
                 ServiceManager.getService(Context.STATUS_BAR_SERVICE));
@@ -996,10 +996,12 @@ public abstract class BaseStatusBar extends SystemUI implements
     private static class SettingsObserver extends ContentObserver {
         private Handler mHandler;
         private ContentResolver mResolver;
+        private IWindowManager mWm;
 
-        SettingsObserver(Handler handler) {
+        SettingsObserver(Handler handler, IWindowManager wm) {
             super(handler);
             mHandler = handler;
+            mWm = wm;
         }
 
         void observe(Context context) {
@@ -1018,7 +1020,16 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
+            boolean hasNavBar = false;
+            try {
+                hasNavBar = mWm.hasNavigationBar();
+            } catch (Exception e) {
+            }
+
             if (uri.equals(Settings.System.getUriFor(Settings.System.TABLET_MODE))) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+            } else if (uri.equals(Settings.System.getUriFor(Settings.System.NAVIGATION_CONTROLS))
+                    && hasNavBar) {
                 android.os.Process.killProcess(android.os.Process.myPid());
             } else {
                 mHandler.removeMessages(MSG_RECREATE);
