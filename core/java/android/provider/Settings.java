8e7093ac30a296df22c4fee04cd7992af34ef872
Scott Brady <sbradymobile@gmail.com>
2012-12-30 22:26:47 -0500

    Multi pane preference settings
    Change-Id: I89679ec7d38ea60ebbc804dd8990ece5c7d9e659

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3068,6 +3068,11 @@ public final class Settings {
         public static final String TABLET_MODE = "tablet_mode";
 
         /**
+         * @hide
+         */
+        public static final String DUAL_PANE_PREFS = "dual_pane_prefs";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
