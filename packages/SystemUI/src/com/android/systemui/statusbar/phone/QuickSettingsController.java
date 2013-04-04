e6e1bda663c20c4ecaa700ba5422985d89c8fa12
Scott Brady <sbradymobile@gmail.com>
2013-04-04 11:00:49 -0400

    No handler in VolumeTile constructor
    Change-Id: I215821df8a4ab0b1983f6c74f731371250be111a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -239,7 +239,7 @@ public class QuickSettingsController {
             } else if (tile.equals(TILE_QUIETHOURS)) {
                 qs = new QuietHoursTile(mContext, inflater, mContainerView, this);
             } else if (tile.equals(TILE_VOLUME)) {
-                qs = new VolumeTile(mContext, inflater, mContainerView, this, mHandler);
+                qs = new VolumeTile(mContext, inflater, mContainerView, this);
             } else if (tile.equals(TILE_EXPANDEDDESKTOP)) {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.EXPANDED_DESKTOP_STYLE));
                 if (expandedDesktopEnabled(resolver)) {
