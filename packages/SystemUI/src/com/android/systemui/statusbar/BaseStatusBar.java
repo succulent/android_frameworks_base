4b9b557bba288722f496812fd9d052355bd22efc
Scott Brady <sbradymobile@gmail.com>
2013-04-05 10:32:10 -0400

    Don't rely on expanded desktop state for pie controls
    Change-Id: If42efbe0e17ee044215077854ffc5b6284f6fc55

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1460,7 +1460,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         int pie = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.PIE_CONTROLS, 0);
 
-        return (pie == 1 && mExpandedDesktopState != 0) || pie == 2;
+        return pie > 0;
     }
 
     private void attachPie() {
