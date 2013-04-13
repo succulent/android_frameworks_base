8aec955ef2a38d8fa1d28506d1a132dbcdcfb8f1
Scott Brady <sbradymobile@gmail.com>
2013-04-13 15:32:58 -0400

    Don't adjust action bar in fullscreen mode
    Change-Id: I154ae5486bf4bd6ea402a5513e2b047a8c423674

--- core/java/com/android/internal/widget/ActionBarOverlayLayout.java
+++ core/java/com/android/internal/widget/ActionBarOverlayLayout.java
@@ -21,6 +21,7 @@ import com.android.internal.app.ActionBarImpl;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Rect;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -155,8 +156,12 @@ public class ActionBarOverlayLayout extends FrameLayout {
         final int vis = getWindowSystemUiVisibility();
         final boolean stable = (vis & SYSTEM_UI_FLAG_LAYOUT_STABLE) != 0;
 
+        final boolean fullscreenMode = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.FULLSCREEN_MODE, 0) == 1;
+
         // The top and bottom action bars are always within the content area.
-        boolean changed = applyInsets(mActionBarTop, insets, true, true, false, true);
+        boolean changed = fullscreenMode ? false :
+                applyInsets(mActionBarTop, insets, true, true, false, true);
         if (mActionBarBottom != null) {
             changed |= applyInsets(mActionBarBottom, insets, true, false, true, true);
         }
