a1ddfb24823d38841d9064ff4286634825716047
Scott Brady <sbradymobile@gmail.com>
2013-07-31 14:10:04 -0400

    Use old volume controller from sgt7
    Change-Id: Id96680fca3e819a75a41c7c5ccb963e9ad08530d

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/VolumeController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/VolumeController.java
@@ -20,59 +20,44 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.os.Vibrator;
 import android.media.AudioManager;
 import android.provider.Settings;
 import android.util.Slog;
 import android.view.IWindowManager;
-import android.widget.CompoundButton;
 
 import com.android.systemui.settings.ToggleSlider;
 
 public class VolumeController implements ToggleSlider.Listener {
     private static final String TAG = "StatusBar.VolumeController";
-    private static final int STREAM = AudioManager.STREAM_NOTIFICATION;
+    private static final int STREAM_MUSIC = AudioManager.STREAM_MUSIC;
+    private static final int STREAM_NOTIF = AudioManager.STREAM_NOTIFICATION;
 
     private Context mContext;
-    private ToggleSlider mControl;
     private AudioManager mAudioManager;
-
-    private boolean mMute;
-    private int mVolume;
-    // Is there a vibrator
-    private final boolean mHasVibrator;
 
     public VolumeController(Context context, ToggleSlider control) {
         mContext = context;
-        mControl = control;
-
-        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        mHasVibrator = vibrator == null ? false : vibrator.hasVibrator();
-
         mAudioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
-
-        mMute = mAudioManager.getRingerMode() != AudioManager.RINGER_MODE_NORMAL;
-        mVolume = mAudioManager.getStreamVolume(STREAM);
-
+        int volume = mAudioManager.getStreamVolume(mAudioManager.isMusicActive() ? STREAM_MUSIC : STREAM_NOTIF);
+        boolean mute = volume < 1;
+        control.setMax(mAudioManager.getStreamMaxVolume(mAudioManager.isMusicActive() ? STREAM_MUSIC : STREAM_NOTIF));
+        control.setValue(volume);
+        control.setChecked(mute);
         control.setOnChangedListener(this);
     }
 
     @Override
     public void onInit(ToggleSlider control) {
-        control.setMax(mAudioManager.getStreamMaxVolume(STREAM));
-        control.setValue(mVolume);
-        control.setChecked(mMute);
     }
 
     public void onChanged(ToggleSlider view, boolean tracking, boolean mute, int level) {
         if (!tracking) {
             if (mute) {
-                mAudioManager.setRingerMode(
-                        mHasVibrator ? AudioManager.RINGER_MODE_VIBRATE
-                                     : AudioManager.RINGER_MODE_SILENT);
+                mAudioManager.setStreamVolume(mAudioManager.isMusicActive() ? STREAM_MUSIC :
+                        STREAM_NOTIF, 0, AudioManager.FLAG_PLAY_SOUND);
             } else {
-                mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-                mAudioManager.setStreamVolume(STREAM, level, AudioManager.FLAG_PLAY_SOUND);
+                mAudioManager.setStreamVolume(mAudioManager.isMusicActive() ? STREAM_MUSIC :
+                        STREAM_NOTIF, level, AudioManager.FLAG_PLAY_SOUND);
             }
         }
     }
