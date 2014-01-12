9dd581ccae6ee30d32c95cee130b7539dcfc2fe1
HD <hdvision16@gmail.com>
2014-01-12 10:07:21 -0600

    Revert "systemui: Add text plain battery style"
    This reverts commit e5d9148ebce390ad3d20b3b7831564e8cdedbde2.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2935,8 +2935,7 @@ public final class Settings {
          * 0: Display the battery an icon in portrait mode
          * 2: Display the battery as a circle
          * 4: Hide the battery status information
-         * 5: Display the battery an icon in landscape mode
-         * 6: Display the battery as plain text
+         * 5: Display the battery an icon  in landscape mode
          * default: 0
          * @hide
          */
